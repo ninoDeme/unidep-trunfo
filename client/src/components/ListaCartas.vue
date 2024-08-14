a77a1fc0555362b1acfa4edf3c984953a73f9137
ricardo.demeterko <ninodemeterko@hotmail.com>
2024-08-14 10:35:30 -0300

    form cartas v1

--- client/src/components/ListaCartas.vue
+++ client/src/components/ListaCartas.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { Modelo } from 'trunfo-lib/models/modelo'
-import { PlusIcon } from '@heroicons/vue/24/outline'
+import { PencilIcon, PlusIcon } from '@heroicons/vue/24/outline'
 import type { CartaTrunfo } from 'trunfo-lib/models/carta'
 import Carta from '@/components/Carta.vue'
 import { ref } from 'vue'
@@ -23,27 +23,6 @@ const emit = defineEmits<{
 
 <template>
   <div class="flex flex-row flex-wrap justify-start gap-5 p-4" v-if="modeloEditando">
-    <div
-      v-for="(carta, index) in cartas"
-      :style="{ height: `${(W_CARTA / 500) * 750}px`, width: `${W_CARTA}px` }"
-      class="relative"
-    >
-      <button
-        type="button"
-        @click="emit('selecionarCarta', carta)"
-        @pointerenter="(hoverEl = index), emit('hoverCarta', carta)"
-        @focus="(hoverEl = index), emit('hoverCarta', carta)"
-        class="bg-gray-700 transition-opacity opacity-0 hover:opacity-100 bg-opacity-40 z-20 absolute w-full h-full left-0 top-0 flex items-center justify-center"
-      >
-        <PlusIcon class="icon" />
-      </button>
-      <Carta
-        :style="{ transform: `scale3d(${W_CARTA / 500}, ${W_CARTA / 500}, 1)` }"
-        :modelo="modeloEditando"
-        :carta="carta"
-        class="origin-top-left"
-      ></Carta>
-    </div>
     <div :style="{ height: `${(W_CARTA / 500) * 750}px`, width: `${W_CARTA}px` }" class="relative">
       <button
         type="button"
@@ -57,6 +36,27 @@ const emit = defineEmits<{
       <Carta
         :style="{ transform: `scale3d(${W_CARTA / 500}, ${W_CARTA / 500}, 1)` }"
         :modelo="modeloEditando"
+        class="origin-top-left saturate-50 blur-sm"
+      ></Carta>
+    </div>
+    <div
+      v-for="(carta, index) in cartas"
+      :style="{ height: `${(W_CARTA / 500) * 750}px`, width: `${W_CARTA}px` }"
+      class="relative"
+    >
+      <button
+        type="button"
+        @click="emit('selecionarCarta', carta)"
+        @pointerenter="(hoverEl = index), emit('hoverCarta', carta)"
+        @focus="(hoverEl = index), emit('hoverCarta', carta)"
+        class="bg-gray-700 transition-opacity opacity-0 hover:opacity-100 bg-opacity-40 z-20 absolute w-full h-full left-0 top-0 flex items-center justify-center"
+      >
+        <PencilIcon class="icon" />
+      </button>
+      <Carta
+        :style="{ transform: `scale3d(${W_CARTA / 500}, ${W_CARTA / 500}, 1)` }"
+        :modelo="modeloEditando"
+        :carta="carta"
         class="origin-top-left"
       ></Carta>
     </div>
